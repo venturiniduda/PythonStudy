69d409989be5421546f12748a6bf339cbd1a47dd
Maria Eduarda Venturini Wollmann <mduduv@gmail.com>
2024-06-20 11:41:33 -0300

    Estruturando o código - Dia 1 - SINCABAP

---
+++ sincabap_proj1.py
@@ -0,0 +1,18 @@
+# bibliotecas 
+
+
+# classe principal
+class main():
+    def receive_data():
+        print("Recebeu dados.")
+
+
+    def modify_data():
+        print("Modificou e padronizou os dados.")
+
+    def send_data():
+        print("Enviou os dados.")
+
+
+# execução
+main.receive_data()
