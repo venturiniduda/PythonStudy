3c4c7d8127f17d76fc34732914153aba6b360dc0
Maria Eduarda Venturini Wollmann <mduduv@gmail.com>
2024-06-20 17:49:54 -0300

    Função Abrir Arquivo - SINCABAP

--- sincabap_proj1.py
+++ sincabap_proj1.py
@@ -1,11 +1,27 @@
-# bibliotecas 
-
+# bibliotecas
+# para receber arquivos:
+import tkinter as tk
+from tkinter import filedialog
 
 # classe principal
-class main():
-    def receive_data():
-        print("Recebeu dados.")
 
+
+class main():
+    def choice_data():
+        filetype = [('Arquivos CSV', '*.csv'),
+                    ('Arquivos Excel', '*.xlsx;*.xls')]
+        ti_data = filedialog.askopenfilename(filetypes=filetype)
+        if ti_data:
+            global arquivo_selecionado
+            arquivo_selecionado = ti_data
+            label_arquivo.config(
+                text=f"Arquivo selecionado: {arquivo_selecionado}")
+        else:
+            label_arquivo.config(text="Nenhum arquivo selecionado.")
+
+    def receive_data():
+
+        print("Recebeu dados.")
 
     def modify_data():
         print("Modificou e padronizou os dados.")
@@ -15,4 +31,22 @@ class main():
 
 
 # execução
+# estruturando tela
+root = tk.Tk()
+# root.withdraw()
+root.title("Escolher arquivo: ")
+arquivo_selecionado = None
+
+label_arquivo = tk.Label(
+    root, text="Nenhum arquivo selecionado.", pady=10, padx=10)
+label_arquivo.pack(fill=tk.BOTH, padx=20, pady=5)
+
+btn_escolher = tk.Button(root, text="Escolher Arquivo: ",
+                         command=main.choice_data, bg = "grey", pady=3, padx=3)
+btn_escolher.pack(fill=tk.BOTH, padx=10, pady=10)
+
+root.mainloop()
+
+print("Caminho do arquivo selecionado: ", arquivo_selecionado)
+
 main.receive_data()
