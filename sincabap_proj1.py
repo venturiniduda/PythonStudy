a2fd3406b9371e1dcf028a1853e6f966c4baf4a3
Maria Eduarda Venturini Wollmann <mduduv@gmail.com>
2024-06-27 14:42:55 -0300

    Ajuste API - SINCABAP

--- sincabap_proj1.py
+++ sincabap_proj1.py
@@ -11,6 +11,16 @@ import flask_restful
 import json
 import os
 
+# classe principal
+class main():
+    def formatar_parametros(body):
+        print("Formatando parâmetros...")
+        return body[0]['datetime']
+
+    def run():
+        print("Iniciou.")
+
+
 # parâmetros para a API
 port = 5723
 api = flask.Flask(__name__)
@@ -20,18 +30,20 @@ getApi = flask_restful.Api(api)
 @api.route('/', methods=['GET'])
 async def raiz():
 
+    # Criando e iniciando API
     os.system('cls')
     print(flask.request.data)
     get_body = json.loads(flask.request.data)
 
     try:
-        datetime_noformat = get_body['datetime']
-        datetime_obj = datetime_noformat.strptime(datetime_noformat, '%y%m%d')
-        formatted_date = datetime_obj.strftime('%d/%m/%y')
-        response = {'datetime_formatted': formatted_date}
+        # datetime_noformat = get_body['datetime']
+        # datetime_obj = datetime_noformat.strptime(datetime_noformat, '%y%m%d')
+        # formatted_date = datetime_obj.strftime('%d/%m/%y')
+        datetime = main.formatar_parametros(get_body)
+        response = "datetime"
         return {
             "message": "Sucesso",
-            "datetime_formatted": formatted_date
+            "datetime_formatted": datetime
         }
 
     except:
