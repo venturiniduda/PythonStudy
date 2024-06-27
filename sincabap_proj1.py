f93f1ba533886742e7468455f23f7537b93e6e78
Maria Eduarda Venturini Wollmann <mduduv@gmail.com>
2024-06-27 16:44:23 -0300

    API Funcional - SINCABAP

--- sincabap_proj1.py
+++ sincabap_proj1.py
@@ -2,20 +2,51 @@
 # para receber arquivos:
 # import tkinter as tk
 # from tkinter import filedialog
-# import csv
-# import datetime as dt
+import csv
 
 # para se conectar ao SAP:
 import flask
 import flask_restful
 import json
 import os
+from datetime import datetime
 
 # classe principal
+
+
 class main():
     def formatar_parametros(body):
         print("Formatando parâmetros...")
-        return body[0]['datetime']
+        data_format = datetime.strptime(
+            body[0]['datetime'], '%Y%m%d').strftime('%d/%m/%Y')
+        return data_format
+
+    def busca_arquivos(p_data):
+        print("Buscando arquivos...")
+
+        diretorio = '/Users/sraso/OneDrive/Documentos/docs_maria/python/diretorio_teste'
+
+        # Buscando arquivos na data recebida
+        for nome_arquivo in os.listdir(diretorio):
+            caminho_arquivo = os.path.join(diretorio, nome_arquivo)
+            # Verificar se não é um diretório
+            if os.path.isfile(caminho_arquivo):
+                data_modificacao = datetime.fromtimestamp(
+                    os.path.getmtime(caminho_arquivo))
+                data_modif = datetime.strftime(data_modificacao, '%d/%m/%Y')
+                if data_modif == p_data:
+                    with open(caminho_arquivo, 'r') as arquivo:
+                        conteudo = arquivo.read()
+                        print(nome_arquivo)
+                        print(conteudo)
+                        result = (f"{nome_arquivo} -> conteudo: {conteudo}")
+                    break
+
+        else:
+            result = (
+                f"Nenhum arquivo encontrado com data {p_data} no diretorio {diretorio}.")
+
+        return result
 
     def run():
         print("Iniciou.")
@@ -36,14 +67,14 @@ async def raiz():
     get_body = json.loads(flask.request.data)
 
     try:
-        # datetime_noformat = get_body['datetime']
-        # datetime_obj = datetime_noformat.strptime(datetime_noformat, '%y%m%d')
-        # formatted_date = datetime_obj.strftime('%d/%m/%y')
+
         datetime = main.formatar_parametros(get_body)
-        response = "datetime"
+        lv_return = main.busca_arquivos(datetime)
+
         return {
             "message": "Sucesso",
-            "datetime_formatted": datetime
+            "datetime_formatted": datetime,
+            "return": lv_return
         }
 
     except:
