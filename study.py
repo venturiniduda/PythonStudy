a1bc81368be931a6b19c60c366d2b3a9a431bde6
Maria Eduarda Venturini Wollmann <mduduv@gmail.com>
2024-07-02 14:08:59 -0300

    Alterações Debbug

--- study.py
+++ study.py
@@ -1,5 +1,6 @@
 # Código para estudos
 # feito por Maria Eduarda Venturini Wollmann
-
+import sys
 print("Olá Mundo! :)")
 print("Esse código é apenas para testar se realmente aprendi a sincronizar o Vs Code com o GitHub.")
+print(sys.version)
